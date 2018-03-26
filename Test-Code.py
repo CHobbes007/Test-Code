e05ec8d37d9c3d3a2dd4f8a958c67670bc901b8f
CHobbes007 <30755959+CHobbes007@users.noreply.github.com>
2018-03-26 16:41:23 -0500

    Test Code

--- Test-Code.py
+++ Test-Code.py
@@ -1,1 +1,15 @@
-I will be using this code to illustrate how to use functions, pass arguments and return variables.
+# I will be using this code to illustrate how to use functions, pass arguments and return variables.
+
+def sumProblem(x, y):
+    sum = x + y
+    sentence = 'The sum of {} and {} is {}.'.format(x, y, sum)
+    print(sentence)
+
+def main():
+    sumProblem(2, 3)
+    sumProblem(1234567890123, 535790269358)
+    a = int(input("Enter an integer: "))
+    b = int(input("Enter another integer: "))
+    print(sumProblem(a, b))
+
+main()
